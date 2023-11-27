9288a708f42fedc0ae5e8dce3aa729e0f287a801
evelynpaixaodida <32101017+evelynpaixaodida@users.noreply.github.com>
2023-11-27 20:54:33 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial/nome.js
@@ -0,0 +1,10 @@
+alert('Bem-vindo ao nosso site!');
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro Preencha todos os campos';
+alert(mensagemDeErro);
+let nome = prompt('Digite o seu nome');
+let idade = prompt('Digite a sua idade');
